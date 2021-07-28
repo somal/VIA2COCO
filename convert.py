57783e940613f02c0fd9ac2a44ab385f91e029d9
Maxim Sokolov <somal1996@rambler.ru>
2021-07-29 00:48:35 +0300

    Refactoring (#1)
    Refactoing code and adapt to me

--- convert.py
+++ convert.py
@@ -1,126 +1,139 @@
-import os
-import cv2
-import datetime
+import csv
 import json
-import getArea
+from datetime import datetime
+from os.path import join
+from typing import Tuple
+
+import cv2
 
 
-
-def create_image_info(image_id, file_name, image_size,
-                      date_captured=datetime.datetime.utcnow().isoformat(' '),
-                      license_id=1, coco_url="", flickr_url=""):
-
-    image_info = {
-            "id": image_id,
-            "file_name": file_name,
-            "width": image_size[0],
-            "height": image_size[1],
-            "date_captured": date_captured,
-            "license": license_id,
-            "coco_url": coco_url,
-            "flickr_url": flickr_url
+def create_image_info(image_id: int, file_name: str, image_size: Tuple[int, int],
+                      date_captured: datetime = datetime.utcnow().isoformat(' '),
+                      license_id: int = 1, coco_url: str = "", flickr_url: str = ""):
+    return {
+        "id": image_id,
+        "file_name": file_name,
+        "width": image_size[0],
+        "height": image_size[1],
+        "date_captured": date_captured,
+        "license": license_id,
+        "coco_url": coco_url,
+        "flickr_url": flickr_url
     }
 
-    return image_info
 
-def create_annotation_info(annotation_id, image_id, category_id, is_crowd,
-                           area, bounding_box, segmentation):
-
-    annotation_info = {
+def create_annotation_info(annotation_id: int, image_id: int, category_id: int, is_crowd: int,
+                           area: float,
+                           bounding_box: Tuple[float, float, float, float], segmentation):
+    return {
         "id": annotation_id,
         "image_id": image_id,
         "category_id": category_id,
         "iscrowd": is_crowd,
-        "area": area,# float
-        "bbox": bounding_box,# [x,y,width,height]
-        "segmentation": segmentation# [polygon]
+        "area": area,  # float
+        "bbox": bounding_box,  # [x,y,width,height]
+        "segmentation": segmentation  # [polygon]
     }
 
-    return annotation_info
 
-
-def get_segmenation(coord_x, coord_y):
-    seg = []
-    for x, y in zip(coord_x, coord_y):
-        seg.append(x)
-        seg.append(y)
-    return [seg]
-
-
-def convert(imgdir, annpath):
-    '''
-    :param imgdir: directory for your images
-    :param annpath: path for your annotations
+def convert(image_dir: str, annotation_path: str):
+    """
+    :param str image_dir: directory for your images
+    :param str annotation_path: path for your annotations
     :return: coco_output is a dictionary of coco style which you could dump it into a json file
     as for keywords 'info','licenses','categories',you should modify them manually
-    '''
-    coco_output = {}
-    coco_output['info'] = {
-        "description": "Example Dataset",
-        "url": "https://github.com/waspinator/pycococreator",
-        "version": "0.1.0",
-        "year": 2018,
-        "contributor": "waspinator",
-        "date_created": datetime.datetime.utcnow().isoformat(' ')
+    """
+
+    # Get the name(type) and supercategory(super_type) from VIA ANNOTATION
+
+    print('Start to reading')
+    rows = []
+    with open(annotation_path, newline='') as csvfile:
+        annotation_reader = csv.DictReader(csvfile)
+        for row in annotation_reader:
+            rows.append(row)
+    print(rows[0])
+
+    print('Collect all categories')
+    # Collect all categories
+    category_names = set([])
+    for row in rows:
+        region_attributes = row['region_attributes']
+        region_attributes = json.loads(region_attributes)
+        if 'type' in region_attributes:
+            category_names.update([region_attributes['type']])
+    coco_categories = [{'id': i + 1, 'name': category, 'supercategory': ''} for i, category in
+                       enumerate(category_names)]
+
+    # Template for output
+    coco_output = {
+        'info': {
+            "description": "Example Dataset",
+            "url": "https://github.com/somal/VIA2COCO",
+            "version": "0.1.0",
+            "year": 2021,
+            "contributor": "somal",
+            "date_created": datetime.utcnow().isoformat(' ')},
+        'licenses': [
+            {
+                "id": 1,
+                "name": "Attribution-NonCommercial-ShareAlike License",
+                "url": "https://creativecommons.org/licenses/by-nc-sa/2.0/"
+            }
+        ],
+        'categories': coco_categories,
+        'images': [],
+        'annotations': []
     }
-    coco_output['licenses'] = [
-        {
-            "id": 1,
-            "name": "Attribution-NonCommercial-ShareAlike License",
-            "url": "http://creativecommons.org/licenses/by-nc-sa/2.0/"
-        }
-    ]
-    coco_output['categories'] = [{
-        'id': 1,
-        'name': 'rib',
-        'supercategory': 'bone',
-    },
-        {
-            'id': 2,
-            'name': 'clavicle',
-            'supercategory': 'bone',
-        }
-    ]
-    coco_output['images'] = []
-    coco_output['annotations'] = []
 
-    ann = json.load(open(annpath))
-    # annotations id start from zero
-    ann_id = 0
-    #in VIA annotations, keys are image name
-    for img_id, key in enumerate(ann.keys()):
-
-        filename = ann[key]['filename']
-        img = cv2.imread(imgdir+filename)
+    print('Parsing')
+    annotation_id = 0
+    for image_id, row in enumerate(rows):
+        filename = row['filename']
+        img = cv2.imread(join(image_dir, filename))
+        if img is None:
+            continue
         # make image info and storage it in coco_output['images']
-        image_info = create_image_info(img_id, os.path.basename(filename), img.shape[:2])
+        image_info = create_image_info(image_id, filename, img.shape[:2])
         coco_output['images'].append(image_info)
-        regions = ann[key]["regions"]
-        # for one image ,there are many regions,they share the same img id
-        for region in regions:
-            cat = region['region_attributes']['label']
-            assert cat in ['rib', 'clavicle']
-            if cat == 'rib':
-                cat_id = 1
-            else:
-                cat_id = 2
-            iscrowd = 0
-            points_x = region['shape_attributes']['all_points_x']
-            points_y = region['shape_attributes']['all_points_y']
-            area = getArea.GetAreaOfPolyGon(points_x, points_y)
-            min_x = min(points_x)
-            max_x = max(points_x)
-            min_y = min(points_y)
-            max_y = max(points_y)
-            box = [min_x, min_y, max_x-min_x, max_y-min_y]
-            segmentation = get_segmenation(points_x, points_y)
-            # make annotations info and storage it in coco_output['annotations']
-            ann_info = create_annotation_info(ann_id, img_id, cat_id, iscrowd, area, box, segmentation)
-            coco_output['annotations'].append(ann_info)
-            ann_id = ann_id + 1
-            
+
+        region_attributes = row['region_attributes']
+        region_attributes = json.loads(region_attributes)
+        if not 'type' in region_attributes:
+            continue
+        category = region_attributes['type']
+        # cate must in categories
+        assert category in category_names
+        # get the cate_id
+        cate_id = 0
+        for category in coco_output['categories']:
+            if category == category['name']:
+                cate_id = category['id']
+        ####################################################################################################
+
+        iscrowd = 0
+        box_dict = row['region_shape_attributes']
+        box_dict = json.loads(box_dict)
+
+        x, y, w, h = box_dict['x'], box_dict['y'], box_dict['width'], box_dict['height']
+        box = (x, y, w, h)
+        area = w * h
+        segmentation = [(x, y), (x + w, y), (x + w, y + h), (x, y + h)]
+        ann_info = create_annotation_info(annotation_id, image_id, cate_id, iscrowd, area, box, segmentation)
+        coco_output['annotations'].append(ann_info)
+        annotation_id += 1
+    print('Finish')
+
     return coco_output
 
 
+if __name__ == '__main__':
+    IMG_FOLDER_PATH = '../bd_presales/cable_line/datasets/21'
+    ANNOTATIONS_FILE_PATH = join(IMG_FOLDER_PATH, 'lbl', 'via_annotation2.csv')
 
+    # Convert VIA annotations to COCO annotations
+    annotations = convert(image_dir=IMG_FOLDER_PATH, annotation_path=ANNOTATIONS_FILE_PATH)
 
+    # Save COCO annotations
+    with open(join(IMG_FOLDER_PATH, 'COCO_annotation.json'), 'w', encoding="utf-8") as outfile:
+        json.dump(annotations, outfile, sort_keys=True, indent=4, ensure_ascii=False)
